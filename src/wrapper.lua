01251242270ddbadea34275c0e31c31d3618b27b
TeknosQuet <teknosgamer58@protonmail.com>
2023-01-29 16:45:01 -0600

    add xdg support & add help menu

--- src/wrapper.lua
+++ src/wrapper.lua
@@ -32,6 +32,32 @@ local function file_exists(loc)
 end
 
 
+-- Display help menu
+local function help()
+	print("Usage: fermenter [-ir] [roblox]  OR  fermenter -b [data]\n")
+	print("-b	boot Roblox (used by generated .desktop file)")
+	print("-h	display this menu")
+	print("-i	install Roblox or modifications")
+	print("-r	remove Roblox or modifications")
+end
+
+-- Create .desktop file
+local function create_desktop_file()
+	local file = io.open(userHome.."/.local/share/applications/fermenter.desktop","w")
+	local contents = {"[Desktop Entry]",
+	"Name=Fermenter","Type=Application","Version=3.14",
+	"Comment=Roblox wrapper for Linux and FreeBSD.",
+	"Exec=/bin/sh -c 'fermenter -b %u'"}
+	
+	-- Write contents of array into file
+	for i,v in pairs(contents) do
+		file:write(v, "\n")
+	end
+
+	-- Add XDG capability
+	os.execute("xdg-mime default fermenter.desktop x-scheme-handler/roblox-player")
+end
+
 -- Install program
 local function install(name)
 	local ver = http.request("http://setup.roblox.com/version")
@@ -58,6 +84,7 @@ local function install(name)
 
 		local envVars = "WINEDEBUG=-all WINEPREFIX="..winePfx.." WINEARCH=win32 WINE_NO_WOW64=1 "
 		os.execute("sh -c '"..envVars.."wine "..tempDir.."/rbx.exe > /dev/null'")
+		create_desktop_file()
 
 		print("Roblox has been installed successfully!")
 	end
@@ -65,6 +92,14 @@ end
 
 -- Uninstall program
 local function uninstall()
+	local desktopFile = userHome.."/.local/share/applications/fermenter.desktop"
+
+	-- Check for desktop file
+	if file_exists(desktopFile) then
+		print ("Removing fermenter.desktop")
+		os.execute("rm -f "..desktopFile)
+	end
+
 	-- Check for existing prefix
 	if file_exists(winePfx) then
 		print("Uninstalling Roblox...")
@@ -77,9 +112,16 @@ local function uninstall()
 end
 
 
+local function boot(data)
+	local envVars = "WINEDEBUG=-all WINEPREFIX="..winePfx.." "
+	local ver = http.request("http://setup.roblox.com/version")
+
+	os.execute("sh -c '"..envVars.."wine "..winePfx.."/drive_c/Program\\ Files/Roblox/Versions/version-*/RobloxPlayerLauncher.exe "..data.."'")
+end
+
 -- Check arguments
-if arg[1] == "install" then
-	if arg[2] == "roblox" then install("roblox") end
-elseif arg[1] == "uninstall" then
-	uninstall()
+if arg[1] == "-b" then boot(arg[2])
+elseif arg[1] == "-h" then help()
+elseif arg[1] == "-i" then install(arg[2])
+elseif arg[1] == "-r" then uninstall()
 end
